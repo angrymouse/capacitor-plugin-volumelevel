a0f69a5cc193c943081980c9cd3a09f4e434f804
angrymouse <rykov.nikas@gmail.com>
2024-12-27 19:40:44 +0100

    ios version

--- ios/Sources/VolumeLevelPlugin/VolumeLevel.swift
+++ ios/Sources/VolumeLevelPlugin/VolumeLevel.swift
@@ -1,8 +1,17 @@
 import Foundation
+import AVFoundation
 
 @objc public class VolumeLevel: NSObject {
-    @objc public func echo(_ value: String) -> String {
-        print(value)
-        return value
+    @objc public func getVolume() -> [String: Any] {
+
+        let currentVolume = AVAudioSession.sharedInstance().outputVolume
+        let maxVolume: Float = 1.0
+        let volumePercentage = currentVolume * 100.0
+        
+        return [
+            "volume": currentVolume,
+            "maxVolume": maxVolume,
+            "volumePercentage": volumePercentage
+        ]
     }
 }
